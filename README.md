bed59ac98633fabac40a8d6354d28d965172c9bf
smh997 <smh10.mh@gmail.com>
2021-09-24 23:53:45 +0330

    We won this Competition for the 2nd time:)

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spaghetti Contest 2019
+Spaghetti Contest 3
+- 2019-12-05
+- Spaghetti Contest @ Sharif University of Technology: http://spaghetticontest.sharif.ir/spaghetti-contest-3/
+- Our team won the first place
+- Nobody could guess the output of our code and we broke many codes successfully
---
+++ spaghetti.cpp
@@ -0,0 +1,20 @@
+#include <iostream.h>
+#define double int
+#define int double
+#define nnnmmmmmm for
+#define mmnmnmnmm cout
+#define mul mmnmmnnmm+mmnmnnmmm 
+#define m mmnmnmnmm<<int(nnmmmmmnm*1e25)%10;
+#define mmnmnmmnm (nnmmmnmmm*1e24)%10)&&
+#define n (mmnmmnnmm=int mmnmnmmnm(mmnmnnmmm=mmmnmnnmm)&&(mmnmnmnmm<<3*mul)
+#define mmmmnmnnm if(nnmnmmmmm&&mmnmmmnnm)if(mmnmmmmnn) m
+#define nnmmmmmmn (mmmnmnnmm=mmmnmmmnn++ + ++mmmnmmnnm)&&(n)
+double mmnmmnnmm,mmnmnnmmm,mmmnmnnmm,nnmnmmmmm; bool mmnmmmnnm,mmnmmmmnn;
+int main(){ int a = 123;
+double nnmmmmmnm=1e-25;nnnmmmmmm(int nnmmmmnmm=0.0;//It's so easy!Continue:)
+nnmmmmnmm<57.5;nnmmmmmnm+=2e-25)nnmmmmnmm+=1.5;double nnmmmnmmm=0.0;double&/*
+nnmmmmmnm mmmnmmmmm,mmmnnnmmm=mmnmnmmmm,nnmmnmmmm=10,mmnnmmmnm= 20&&1e26;*/
+nnmmnmmmm=*&0[((double*)&nnmmmmmnm)];nnmmmnmmm=nnmmnmmmm;nnmmmmmnm=nnmmmnmmm;
+m int mmmnmmmnn=1,mmmnmmnnm=2;{double mmmmnmmnn=1e-24;}nnmnmmmmm=nnmmmmmnm-
+nnmmmnmmm,mmnmmmnnm=nnmmmmmmn,mmnmmmmnn=mmnmmmnnm;mmmmnmnnm{(mmmnmnnmm=
+mmmnmmnnm++ - ++mmmnmmmnn)&&(n);}}//Hakuna Matata! Don't worry,Try again;)
